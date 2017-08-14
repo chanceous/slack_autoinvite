e6535603ff09e976171fce6336aecd4110a1c247
Christopher Chancé <christopherchance67@gmail.com>
2017-08-14 19:02:49 -0300

    Update unirme.php

--- unirme.php
+++ unirme.php
@@ -4,7 +4,7 @@ if(isset($_POST['email'])){
 }else{ header('Location: index.php?error'); }
 $email = $_POST['email'];
 // Aca Abajo Ingresen el Token Obtenido en la URL del Repositorio
-$token = 'xoxp-179688677408-181108932913-227356257382-99932137a97e2bceafc1b2a7973c255e';
+$token = 'ACA EL TOKEN OBTENIDO';
 
 $json = file_get_contents('https://slack.com/api/users.admin.invite?token='.$token.'&pretty=1&email='.$email);
 $obj = json_decode($json);
